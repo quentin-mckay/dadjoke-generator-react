a7c38c54cf0c47df60ead76905ae82092c2d6ada
Quentin McKay <quentinmckay1@gmail.com>
2023-04-29 14:46:49 +1000

    added smiley link

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 
+import smiley from './assets/smiley.png'
+
 const App = () => {
     const [topic, setTopic] = useState('')
     const [joke, setJoke] = useState('')
@@ -23,31 +25,34 @@ const App = () => {
         }
     }
 
-	const pickRandom = (arr) => {
-		const randomIndex = Math.floor(Math.random() * arr.length)
-		return arr[randomIndex]
-	}
+    const pickRandom = (arr) => {
+        const randomIndex = Math.floor(Math.random() * arr.length)
+        return arr[randomIndex]
+    }
 
     const generateJoke = async (e) => {
-		e.preventDefault()
+        e.preventDefault()
 
         const jokesArray = await fetchJoke(topic)
 
         if (jokesArray.length === 0) {
             setJoke('Sorry, there are no jokes with that topic.')
         } else {
-			const joke = pickRandom(jokesArray).joke // joke objects are in the form { id, joke }
-            setJoke(joke) 
+            const joke = pickRandom(jokesArray).joke // joke objects are in the form { id, joke }
+            setJoke(joke)
         }
     }
 
     return (
         <div className='bg-slate-800 h-screen text-white grid place-items-center text-xl'>
             <div className='p-8 border-2 border-gray-500 rounded-md min-h-[25%] max-w-3xl bg-white/5'>
-                <h1 className='text-5xl text-center font-semibold'>
+                <h1 className='text-5xl text-center font-semibold '>
                     Dad Joke Generator 3000
                 </h1>
-                <form onSubmit={generateJoke} className='mt-8 w-3/4 mx-auto text-center '>
+                <form
+                    onSubmit={generateJoke}
+                    className='mt-8 w-3/4 mx-auto text-center '
+                >
                     <div className='flex gap-2 items-center justify-center'>
                         <label htmlFor='topic'>Topic:</label>
                         <input
@@ -60,11 +65,17 @@ const App = () => {
                         />
                     </div>
                     <button className='w-3/4 border bg-orange-500/10 border-orange-500 px-4 py-2 mt-8 hover:bg-orange-500/30 active:bg-orange-500/60 rounded-md'>
-						{topic ? `Tell me a joke about ${topic}!` : "Tell me a random joke!"}
-                        
+                        {topic
+                            ? `Tell me a joke about ${topic}!`
+                            : 'Tell me a random joke!'}
                     </button>
                 </form>
-                <p className='mt-6 max-w-[75%] mx-auto text-2xl'>{joke}</p>
+                <p className='mt-6 max-w-[80%] mx-auto text-2xl font-light'>
+                    {joke}
+                </p>
+                <a href='https://icanhazdadjoke.com/api' target="_blank" className='absolute w-10 h-10 bottom-0 left-0'>
+                    <img src={smiley} alt='smiley face' />
+                </a>
             </div>
         </div>
     )
