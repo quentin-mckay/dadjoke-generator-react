ba1c00799556ed64d6cb08bfeeeddaeb2d316420
Quentin McKay <quentinmckay1@gmail.com>
2023-04-29 23:30:18 +1000

    updated font size and text centering

--- src/App.jsx
+++ src/App.jsx
@@ -44,39 +44,39 @@ const App = () => {
     }
 
     return (
-        <div className='bg-slate-800 h-screen text-white grid place-items-center text-xl'>
+        <div className='grid h-screen text-xl text-white bg-slate-800 place-items-center'>
             <div className='w-3/4 max-w-3xl p-8 border-2 border-gray-500 rounded-md bg-white/5'>
-                <h1 className='text-5xl text-center font-semibold '>
+                <h1 className='text-5xl font-semibold text-center '>
                     Dad Joke Generator
                 </h1>
                 <form
                     onSubmit={generateJoke}
-                    className='mt-8 w-3/4 mx-auto text-center '
+                    className='w-3/4 mx-auto mt-8 text-center '
                 >
-                    <div className='flex gap-2 items-center justify-center'>
+                    <div className='flex items-center justify-center gap-2'>
                         <label htmlFor='topic'>Topic:</label>
                         <input
                             type='text'
                             id='topic'
-                            className='px-2 py-1 rounded text-black'
+                            className='px-2 py-1 text-black rounded'
                             placeholder=''
                             value={topic}
                             onChange={(e) => setTopic(e.target.value)}
                         />
                     </div>
-                    <button className='w-3/4 border bg-orange-500/10 border-orange-500 px-4 py-2 mt-8 hover:bg-orange-500/30 active:bg-orange-500/60 rounded-md'>
+                    <button className='w-3/4 px-4 py-2 mt-8 border border-orange-500 rounded-md bg-orange-500/10 hover:bg-orange-500/30 active:bg-orange-500/60'>
                         {topic
                             ? `Tell me a joke about ${topic}!`
                             : 'Tell me a random joke!'}
                     </button>
                 </form>
-                <p className='mt-6 max-w-[80%] mx-auto text-2xl font-light'>
+                <p className='mx-auto mt-6 text-3xl font-light text-center'>
                     {joke}
                 </p>
                 <a
                     href='https://icanhazdadjoke.com/api'
                     target='_blank'
-                    className='absolute w-10 h-10 bottom-0 right-0'
+                    className='absolute bottom-0 right-0 w-10 h-10'
                 >
                     <img src={smiley} alt='smiley face' />
                 </a>
